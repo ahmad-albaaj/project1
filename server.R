e362e0880bf1f9303ec55775437dcfa628a09c23
“ahmad-albaaj” <ahmedb3aj@yahoo.com>
2021-05-25 00:56:05 +0000

    export data as xlsx

--- server.R
+++ server.R
@@ -19,7 +19,6 @@ shinyServer(function(input, output) {
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
     # draw the histogram with the specified number of bins
-    hist(x, main = "AM", xlab = "Farm", ylab = "Freq", breaks = bins, col = 'green', border = 'black')
     hist(x, main = "AM", xlab = "Farm", ylab = "Freq", breaks = bins, col = 'green', border = 'black') 
     axis(1, col = 'blue', col.axis = 'purple', col.ticks = 'red', cex.axis = 1.5, font = 2, family = 'serif')
     axis(2, col = 'maroon', col.axis = 'pink', col.ticks = 'limegreen', cex.axis = 0.9, font =3, family = 'mono')
@@ -27,3 +26,17 @@ shinyServer(function(input, output) {
   })
   
 })
+
+
+#####
+#export data as xlsx
+#mthode 1
+#> install.packages("writexl")
+#> library("writexl")
+#> write_xlsx(faithful,"C:\\Users\\Albaaj\\Desktop\\R Shiny MasterClass\\faithful.xlsx")
+#methode 2
+#> install.packages("xlsx")
+#> library("xlsx")
+#> write.xlsx(faithful, file = "faithful.xlsx",
+#+            sheetName = "Faithful", append = FALSE)
+> 
